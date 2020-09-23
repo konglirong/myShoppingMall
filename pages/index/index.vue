b9aba3310bf60433d380b535693bd7704b668fe8
konglirong <1453931154@qq.com>
2020-09-23 22:01:47 +0800

    首页轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<view class="content">
-		首页
+		<swiper class="swiper" indicator-dots autoplay :interval="1000" circular>
+			<swiper-item v-for="(item,index) in imgList" class="img-item">
+				<image :src="item.src" class="imgStyle"></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,7 +12,19 @@
 	export default {
 		data() {
 			return {
-				 
+				imgList: [{
+					src: "../../static/swipper/img1.jpg",
+					text: "图片1",
+					id: '001'
+				}, {
+					src: "../../static/swipper/img2.jpg",
+					text: "图片2",
+					id: '002'
+				}, {
+					src: "../../static/swipper/img3.jpg",
+					text: "图片3",
+					id: '003'
+				}]
 			}
 		},
 		onLoad() {
@@ -20,6 +36,16 @@
 	}
 </script>
 
-<style>
- 
+<style lang="scss">
+	.content {
+		swiper {
+			width: 750rpx;
+			height: 380rpx;
+			image{
+				width:100%;
+				height: 100%;
+			}
+		}
+	}
+	
 </style>
